83f0e87d3b7e603c96a5bd2b07f34168c7ee8a55
TTumyr <trond.tumyr@online.no>
2020-03-01 18:00:06 +0100

    added components, component styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
