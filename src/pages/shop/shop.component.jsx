c04025b79a930fb705b3a3f4da13bb7861b67972
TTumyr <trond.tumyr@online.no>
2020-03-03 00:04:41 +0100

    update shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import SHOP_DATA from "./shop-data.js";
+import CollectionPreview from "../../components/collection-preview/collection-preview";
 
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      collection: SHOP_DATA
+      collections: SHOP_DATA
     };
   }
   render() {
+    const { collections } = this.state;
     return (
-      <div>
-        <h1>Hello from shop</h1>
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
       </div>
     );
   }
